14da801c352062faa800e9ea39cc5f10f175b728
Olaf <olaf_hahn@at.ibm.com>
2017-01-23 08:49:27 +0100

    Resulution enhancements, split OPENCV and TakePhoto, Defaultpath is Pictures

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # node-red-contrib-camerapi
-A <a href="http://nodered.org" target="_new">Node-RED</a> node to take photos on a Raspberry Pi. This node will only work on an Raspberry Pi with a Raspberry Pi Camera and also utilizes the <a href="http://opencv.org" target="_new">OpenCV</a> Framework to detect faces.
-
+A <a href="http://nodered.org" target="_new">Node-RED</a> node to take photos on a Raspberry Pi. This node will only work on an Raspberry Pi with a Raspberry Pi Camera enabled.
 
 Install
 -------
@@ -9,28 +8,17 @@ Run the following command in the root directory of your Node-RED install or home
 
         npm install node-red-contrib-camerapi
 
+This node was tested on Nodejs V4.x LTS and NPM 2.x on Node-Red. 
+
 ### Additionally you have to install on the Raspberry Pi 
 
-The detect node also utilizes the <b>OpenCV Framework</b> to give some additional capabilities to the photo processing (detect photo). Therefore you have to install it on your Raspberry Pi. 
-Take a look at this <a href="http://www.pyimagesearch.com/2015/07/27/installing-opencv-3-0-for-both-python-2-7-and-python-3-on-your-raspberry-pi-2/" target="_new">tutorial</a> to see how to install.
-
-Don't forget to enable the Raspberry Pi Camera in raspi-config. 
+First you have to install a Raspberry Pi Camera physically and don't forget to enable the Camera in raspi-config. 
+If you are using the default path - the /home/pi/Pictures will be used.
 
 Usage
 -----
 
-Provides some nodes to take a photo, capture faces and delete them.
-
-
 ### TakePhoto
 
 This node is to take a photo in a given format directly from the Raspberry Pi Camera. The image of the photo is stored into the file-system and <b>msg.payload</b> will give you the path and the filename including extension to the photo.
-
-### DetectFaces
-
-If you choose the Face-Detection (based on OpenCV Framework) - you will also get in msg.facescount the number of detected faces and a JSON with the necessary information to the detected face (x,y,x+w,y+h) and if in filemode the name, path and format of the file with the face cut from the image. 
-
-### DeletePhoto
-
-Will delete a taken photo from the given path with or without the extracted faces. 
 
--- camerapi.js
+++ camerapi.js
@@ -51,9 +51,10 @@ module.exports = function(RED) {
 		
          	var fsextra = require("fs-extra");
          	var fs = require("fs");
-         	var uuid = require('node-uuid').v4();
+         	var uuid = require('uuid/v4');
+			var os = require('OS');
          	var localdir = __dirname;
-         	var defdir = '/home/pi/images';
+         	var defdir = os.homedir() + '/Pictures';
             var cl = "python " + localdir + "/lib/python/get_photo.py";
             var resolution;
             var fileformat;
@@ -103,7 +104,7 @@ module.exports = function(RED) {
 	         		if (node.filepath) {
 	         			filepath = node.filepath;
 	         		} else {
-	         			filepath = defdir + "/images/";
+	         			filepath = defdir + "/Pictures/";
 	         		}
 	         	}
 	 			cl += " "+filepath;
@@ -135,8 +136,12 @@ module.exports = function(RED) {
              	cl += " 640 480";          		
          	} else if (resolution == "3" ) {
              	cl += " 800 600";          		
-         	} else  {
+         	} else if (resolution == "4" ) {
              	cl += " 1024 768";          		
+         	} else if (resolution == "5" ) {
+             	cl += " 1920 1080";          		
+         	} else  {
+             	cl += " 2592 1944";          		
          	}
 
          	if ((msg.fliph) && (msg.fliph !== "")) {
@@ -178,7 +183,7 @@ module.exports = function(RED) {
                 //console.log('[exec] stderr: ' + stderr);
                 if (error !== null) {
                     msg3 = {payload:error};
-                    //console.log('[exec] error: ' + error);
+					console.error("CameraPi (err): "+ err);
                     msg.payload = "";
                     msg.filename = "";
                     msg.fileformat = "";
@@ -188,7 +193,7 @@ module.exports = function(RED) {
                     msg.filepath = filepath;
                     msg.fileformat = fileformat;
 
-                    // get the raw image into payload and delete tempfile
+                    // get the raw image into payload and delete tempfile on buffermode
                     if (filemode == "0") {
                     	// put the imagefile into payload
                     	msg.payload = fs.readFileSync(filefqn);
@@ -221,218 +226,4 @@ module.exports = function(RED) {
         });	
     }
 	RED.nodes.registerType("camerapi-takephoto",CameraPiTakePhotoNode);
-
-	// CameraPI Detect Node
-    function CameraPiDetectNode(config) {
-    	// Create this node
-        RED.nodes.createNode(this,config);
-        
-        // set parameters and save locally 
-        this.filemode = config.filemode;
-		this.filename =  config.filename;
-		this.filedefpath = config.filedefpath;
-		this.filepath = config.filepath;
-		this.fileformat = config.fileformat;
-		this.detect = config.detect;
-		this.framesize =  config.framesize;
-		this.extract = config.extract;
-		this.occurance = config.occurance;
-	    this.repeat = config.repeat;
-		this.name =  config.name;
-		this.activeProcesses = {};
-
-		var node = this;
-		
-        // if there is an new input
-		node.on('input', function(msg) {
-			
-         	var fsextra = require("fs-extra");
-         	var fs = require("fs");
-         	var localdir = __dirname;
-         	var defdir = '/home/pi/images';
-            var cl = "python " + localdir + "/lib/python/face_detect.py";
-         	var uuid = require('node-uuid').v4();
-         	var filemode;
-            var filename;
-            var filepath;
-            var fileformat;
-            var filefqn;
-            var detect;
-            var framesize;
-            var extract;
-
-         	node.status({fill:"green",shape:"dot",text:"node-red:common.status.connected"});
-
-         	// Check the given filemode
-         	if((msg.filemode) && (msg.filemode !== "")) {
-         		filemode = msg.filemode;
-         	} else {
-         		if (node.filemode) {
-         			filemode = node.filemode;
-         		} else {
-         			filemode = "1";
-         		}
-         	}
-         		
-         	if (filemode == "0") {
-         		filename = "pic_" + uuid + '.jpg';
-         		fileformat = "jpeg";
-         		filepath = defdir + "/";
-         		filefqn = filepath + filename;
-
-         		if (RED.settings.verbose) { node.log("camerapi detect:"+filefqn); }
-         		console.log("CameraPi (log): Tempfile - " + filefqn);
-
-                // put the raw image into a tempfile if running in buffer mode
-            	fs.writeFileSync(filefqn, msg.payload);
-
-         		cl += " " + filename + " " + filepath + " " + fileformat;
-         	} else {
-             	
-     			if ((msg.filename) && (msg.filename.trim() !== "")) {
-    	     			filename = msg.filename;
-    	    	} else {
-    	    		if (node.filename) {
-    	         		filename = node.filename;
-    	    		} else {
-    	         		filename = "pic_" + uuid + '.jpg';
-    	    		}
-    	    	}
-    			cl += " "+filename;
-    	
-    			if ((msg.filepath) && (msg.filepath.trim() !== "")) {
-         			filepath = msg.filepath;
-             	} else {
-             		if (node.filepath) {
-             			filepath = node.filepath;
-             		} else {
-             			if (detect == "1") {
-                 			filepath = defdir + "/faces/";
-             			} else {
-                 			filepath = defdir + "/objects/";
-             			}
-             		}
-             	}
-     			cl += " "+filepath;
-     	 		     		
-             	if ((msg.fileformat) && (msg.fileformat.trim() !== "")) {
-         			fileformat = msg.fileformat;
-             	} else {
-             		if (node.fileformat) {
-             			fileformat = node.fileformat;
-             		} else {
-             			fileformat = "jpeg";
-             		}
-             	}
-     			cl += " "+fileformat;
-         	}
-
-     		filefqn = filepath + filename;
-
-         	if ((msg.detect) && (msg.detect !== "")) {
-         		detect = msg.detect;
-         	} else {
-         		if (node.detect) {
-             		detect = node.detect;
-         		} else {
-             		detect = "1";
-         		}
-         	}
-     		cl += " " + detect;
-
-         	if ((msg.framesize) && (msg.framesize !== "")) {
-         		framesize = msg.framesize; 
-         	} else {
-         		if (node.framesize) {
-             		framesize = node.framesize;	        			         			
-         		} else {
-             		framesize = "1";	      
-             		}
-         	}
-         	if (framesize == "1") {
-             	cl += " 15 20"; 
-         	} else if (framesize == "2" ) {
-             	cl += " 20 25";          		
-         	} else  {
-             	cl += " 25 30";          		
-         	}
-
-         	if ((msg.extract) && (msg.extract !== "")) {
-         		extract = msg.extract; 
-         	} else {
-         		if (node.extract) {
-             		extract = node.extract;	        			         			
-         		} else {
-             		extract = "0";	      
-             		}
-         	}
-     		cl += " " + extract;
-
-            if (RED.settings.verbose) { node.log(cl); }
-            
-            var child = exec(cl, {encoding: 'binary', maxBuffer:10000000}, function (error, stdout, stderr) {
-                var retjson = {};
-            	var retval = new Buffer(stdout,"binary");
-                try {
-                    if (isUtf8(retval)) { retval = retval.toString(); }
-                } catch(e) {
-                    node.log(RED._("exec.badstdout"));
-                }
-
-                // console.log('camerapi-detect:'+retval);
-
-                msg.faces = [];
-
-                // check error 
-                var msg2 = {payload:stderr};
-                var msg3 = null;
-                //console.log('[exec] stdout: ' + stdout);
-                //console.log('[exec] stderr: ' + stderr);
-                if (error !== null) {
-                    msg3 = {payload:error};
-                    //console.log('[exec] error: ' + error);
-                    msg.payload = 0;
-                    // msg.filename = "";
-                    // msg.filepath = "";
-                    // msg.fileformat = "";
-                } else {
-                    if (detect == "1") {
-                        if (RED.settings.verbose) { node.log('camerapi-detect:'+retval); }
-                        
-                        retjson = JSON.parse(retval);
-                        console.log('camerapi-detect:' + retjson);
-                        msg.facecount = retjson.facecount;
-                        msg.payload = retjson.facecount;
-                    	msg.faces = retjson.faces;
-                    }                	
-
-                    if (filemode == "0") {
-                    	// delete tempfile
-               	   		fsextra.remove(filefqn, function(err) {
-                   		  if (err) return console.error("CameraPi (err): "+ err);
-                   		  console.log("CameraPi (log): " +  filefqn + " remove success!")
-                   		});	           				           			
-                    } else {
-                        msg.payload = filefqn;
-                    }
-                }
-                
-                node.status({});
-                node.send(msg);
-                delete node.activeProcesses[child.pid];
-            });
-            
-            child.on('error',function(){});
-            
-            node.activeProcesses[child.pid] = child;
-         	
-        });
-            
-        // CameraPi-Detect has a close 
-        node.on('close', function(done) {
-        	node.closing = true;
-            done();
-        });	
-    }
-	RED.nodes.registerType("camerapi-detect",CameraPiDetectNode);
 }
